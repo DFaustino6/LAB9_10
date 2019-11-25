45b96120b016ec6591b4ff2fd7777dc9c46289bd
DFaustino6 <a61171@ualg.pt>
2019-11-25 21:02:54 +0000

    com

--- app/Http/Controllers/Blog.php
+++ app/Http/Controllers/Blog.php
@@ -15,10 +15,10 @@ class Blog extends Controller
                 'MENU2' => 'SubForum2',
                 'MENU3' => 'SubForum3',
                 'MENU4' => 'Logout',
-                'href4' => 'logout',
+                'href4' => 'blog/logout',
                 'MENU5' => 'Welcome'.' '.session()->get('name'),
                 'MENU6' => 'Blog',
-                'href6' => 'post',
+                'href6' => 'blog/post',
                 'loginId' => session()->get('id'),
                 'href5' => '#',
                 'db'   => $db
@@ -30,9 +30,9 @@ class Blog extends Controller
                 'MENU2' => 'SubForum2',
                 'MENU3' => 'SubForum3',
                 'MENU4' => 'Login',
-                'href4' => 'login',
+                'href4' => 'blog/login',
                 'MENU5' => 'Register',
-                'href5' => 'register',
+                'href5' => 'blog/register',
                 'db'   => $db
             ); 
         }
@@ -45,9 +45,9 @@ class Blog extends Controller
             'MENU2' => 'SubForum2',
             'MENU3' => 'SubForum3',
             'MENU4' => 'Login',
-            'href4' => 'login',
+            'href4' => 'blog/login',
             'MENU5' => 'Register',
-            'href5' => 'register'
+            'href5' => 'blog/register'
          );  
 
         return view('register_template',$values);
@@ -59,6 +59,7 @@ class Blog extends Controller
             'MENU2' => 'SubForum2',
             'MENU3' => 'SubForum3',
             'MENU4' => 'Login',
+            'MENU5' => 'Register',
             'Msg'   => 'Registration Completed.Welcome!',
             'text_color' => 'green',
             'back_color' => '#00d269',
@@ -71,9 +72,9 @@ class Blog extends Controller
         $nrows=Blog_model::check_email($request->Email);
 
         if(count($nrows)>0)
-            return redirect('register')->withErrors('Email já existe na base de dados')->withInput($request->except('Email'));
+            return redirect('blog/register')->withErrors('Email já existe na base de dados')->withInput($request->except('Email'));
         elseif($request->Pwd!=$request->ConfPwd)
-            return redirect('register')->withErrors('Passwords não coincidem')->withInput();
+            return redirect('blog/register')->withErrors('Passwords não coincidem')->withInput();
         else{
             Blog_model::register_user($Username,$Email,$pwdHash);
             return view('message_template',$values);
@@ -87,9 +88,9 @@ class Blog extends Controller
             'MENU2' => 'SubForum2',
             'MENU3' => 'SubForum3',
             'MENU4' => 'Login',
-            'href4' => 'login',
+            'href4' => 'blog/login',
             'MENU5' => 'Register',
-            'href5' => 'register',
+            'href5' => 'blog/register',
          ); 
 
         return view('login_template',$values);
@@ -125,7 +126,7 @@ class Blog extends Controller
             return view('message_template',$values); 
         } 
         else 
-         return redirect('login')->withErrors('Wrong email or password.'); 
+         return redirect('blog/login')->withErrors('Wrong email or password.'); 
     }
 
     public function logout(){
@@ -142,7 +143,7 @@ class Blog extends Controller
             'back_color' => '#ff9966',
             'icon' => 'glyphicon glyphicon-log-out'
         );
-        Cookie::queue('autologin','',-1);
+        Cookie::queue('siteAuth','',-1);
         return view('message_template',$values);
     }
 
@@ -168,9 +169,9 @@ class Blog extends Controller
         $nrows=Blog_model::get_blog($user_id,$blog_id);
         $values = array(
             'MENU0' => 'Logout',
-            'href0' => 'logout',
+            'href0' => 'blog/logout',
             'MENU1' => 'Welcome'.' '.session()->get('name'),
-            'href1' => 'post',
+            'href1' => 'blog/post',
             'ActionPost' => 'Write',
             'PostId' => '',
             'Post_content' => ''
